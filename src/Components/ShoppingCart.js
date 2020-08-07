c9bea98d7030f866bc1cf7d50d5199e3d028d681
Antonio <tonimagwow@gmail.com>
2020-08-06 23:55:11 -0700

    shoppingcart rendering headers

--- src/Components/ShoppingCart.js
+++ src/Components/ShoppingCart.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 import Title from './Title'
+import CartLayout from './CartLayout'
 
 export default class ShoppingCart extends Component {
     render() {
         return (
-            <section mt-3>
-                <h3>Shopping Cart</h3>
-            </section>
+            <>
+                <section className="mt-5">
+                    <Title name="Shopping Cart" />
+                    
+                </section>
+                <section className="mt-5">
+                    <CartLayout  />
+                </section>
+            </>
         )
     }
 }
